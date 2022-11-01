edc6cd0cdd477d4efb46418f4427d3cc283de9ee
Abhijith KR <abhijithkr7745@gmail.com>
2022-11-01 20:28:03 +0530

    Statifiles dir updated

--- Developer_Api_Oct_Hackathon2022/settings.py
+++ Developer_Api_Oct_Hackathon2022/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 # static folder setup
-STATIC_URL = 'static/'
-STATICFILES_DIRS= [os.path.join(BASE_DIR,'static')]
+STATIC_URL = '/static/'
+# STATICFILES_DIRS= [os.path.join(BASE_DIR,'static')]
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 # media folder setup
